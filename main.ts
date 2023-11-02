9d3173177d6dec6b2552803721fe68972e2a34fa
Arjun313w <135212653+Arjun313w@users.noreply.github.com>
2023-11-02 16:00:24 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    record.setSampleRate(10000, record.AudioSampleRateScope.Recording)
+    basic.showIcon(IconNames.SmallSquare)
+    record.startRecording(record.BlockingState.Blocking)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    record.setSampleRate(20000, record.AudioSampleRateScope.Playback)
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        . # # # .
+        . # # . .
+        . # . . .
+        `)
+    record.playAudio(record.BlockingState.Blocking)
+    basic.clearScreen()
+})
